7af7c60b302b27e3ef86a5a7b4b5ce133b782385
joshdeleon-dev <85369351+joshdeleon-dev@users.noreply.github.com>
2025-12-04 02:10:46 +0000

    css fix 2

--- index.tsx
+++ index.tsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './src/index.css';
 
+console.log('HireRevOps App Loading...');
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
+  console.error("Could not find root element to mount to");
   throw new Error("Could not find root element to mount to");
 }
+
+console.log('Root element found, rendering app...');
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
@@ -14,3 +19,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+console.log('HireRevOps App Rendered!');
